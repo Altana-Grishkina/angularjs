964e5ba30b2f1e6d22f2b15b9cc8544c49cd44f7
Altana <altanagrishkina@gmail.com>
2023-07-25 21:34:00 +0200

    Interpolation, dependency injection, filters

--- coursera-projects/lectures/lecture11/app.js
+++ coursera-projects/lectures/lecture11/app.js
@@ -1,25 +1,50 @@
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    angular.module('MsgApp', [])
+  angular
+    .module("MsgApp", [])
 
-    .controller('MsgController', MsgController);
-       
-    MsgController.$inject = ['$scope'];
-    function MsgController($scope) {
-        $scope.name = "Yaakov";
-        $scope.stateOfBeing = "hungry";
+    .controller("MsgController", MsgController)
+    .filter('loves', LovesFilter)
+    .filter('truth', TruthFilter);
 
-        $scope.sayMessage = function() {
-            return "Yakov likes to eat";
-        };
+  MsgController.$inject = ["$scope", "lovesFilter"];
+  function MsgController($scope, lovesFilter) {
+    $scope.name = "Yaakov";
+    $scope.stateOfBeing = "hungry";
+    $scope.cookieCost = 0.45;
+    $scope.sayMessage = function () {
+      var msg = "Yakov likes to eat";
+      return msg;
+    };
 
-        $scope.feed = function() {
-            $scope.stateOfBeing = "fed";
-        };
+    $scope.sayLovesMessage = function () {
+      var msg = "Yakov likes to eat healthy at the night";
+      msg = lovesFilter(msg);
+      return msg;
+    };
+
+    // https://docs.angularjs.org/api/ng/filter/uppercase
+
+    $scope.feed = function () {
+      $scope.stateOfBeing = "fed";
+    };
+  }
+
+  function LovesFilter(){
+    return function(input) {
+      input = input  || "";
+      input = input.replace("likes", "loves");
+      return input;
+    };
+  }
+
+  function TruthFilter() {
+    return function(input, target, replace) {
+      input = input  || "";
+      input = input.replace(target, replace);
+      return input;
     }
-
-
-       
+  }
 
 })();
