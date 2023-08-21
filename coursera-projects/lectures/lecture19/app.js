d2f812651846a177c420fdbed28fc3207a3d5af9
Altana <altanagrishkina@gmail.com>
2023-08-22 01:20:02 +0200

    Lecture 19, Part 2: Prototypal Inheritance

---
+++ coursera-projects/lectures/lecture19/app.js
@@ -0,0 +1,22 @@
+(function () {
+  "use strict";
+
+  angular.module('ShoppingListApp', [])
+    .controller('ShoppingListController', ShoppingListController);
+
+    ShoppingListController.$inject = ['$scope'];
+    function ShoppingListController($scope){
+
+      $scope.shoppingList1 = shoppingList1;
+      $scope.shoppingList2 = shoppingList2;
+
+      $scope.addToList = function (){
+        var newItem = {
+          name: $scope.newItemName,
+          quantity: $scope.newItemQuantity
+        };
+      };
+
+      $scope.shoppingList2.push(newItem);
+    };
+})();
